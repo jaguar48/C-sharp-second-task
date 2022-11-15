dbd5efd6b954b13d5ab532e1b9748cb20676b713
Stanley Okonkwo <okonkwostanley67@yahoo.com>
2022-11-15 00:18:00 -0800

    Update leapyear.cs

--- leapyear.cs
+++ leapyear.cs
@@ -1,36 +1,28 @@
-using System;
 
-namespace leapchecker{
+
+using System;
+namespace leapyear{
     public class HelloWorld
 {
     
-    public class yearcount{
-        public int current_year;
-        public int count_years ;
+    public static void calcleap(){
+        int year, count= 0;
+        Console.WriteLine("Enter year");
         
-        public void 
+        year = int.Parse(Console.ReadLine());
         
-        public yearcount(int current, int countYear){
-            current_year = current;
-            count_years = countYear;
-            
-             while ( countYear< 20 ){
-                current = current +1;
-                if ((current % 4 == 0)){
-                    countYear++;
-                    Console.WriteLine(current);
-                }
+        while (count<20){
+            year ++;
+            if ((year %4 ==0 )|| (year % 400 ==0)){
+                Console.WriteLine("year" + year + "is a leap year");
+                count ++;
             }
+            
         }
-
     }
     public static void Main(string[] args)
     {
-        
-    yearcount countye = new yearcount(2022,0);
-  
+        calcleap();
     }
 }
 }
-
- 
